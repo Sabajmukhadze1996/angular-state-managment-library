a32884e26dd5955360fbf71d3c004b8907ea5b6a
saba_jmukhadze <sabajmukhadze23@gmail.com>
2023-04-25 02:45:32 +0400

    NGRX updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CounterComponent } from './counter/counter/counter.component';
 import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
 import { counterReducer } from './counter/state/counter.reducer';
+import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { counterReducer } from './counter/state/counter.reducer';
     CounterComponent,
     CounterButtonsComponent,
     CounterOutputComponent,
+    CustomCounterInputComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,22 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { CounterState } from '../state/counter.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
-  styleUrls: ['./counter-output.component.css']
+  styleUrls: ['./counter-output.component.css'],
 })
 
-
 export class CounterOutputComponent implements OnInit {
-  counter!: number;
+  counter$!: Observable<{ counter: number }>;
 
-  constructor(private store: Store<{counter: {counter: number}}>) {}
+  constructor(private store: Store<{ counter: CounterState }>) {}
 
   ngOnInit(): void {
-      this.store.select('counter').subscribe(data => {
-        this.counter = data.counter;
-      })
+    this.counter$ = this.store.select('counter');
   }
-
+  
 }
